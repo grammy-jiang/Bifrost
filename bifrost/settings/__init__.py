af1e48e07790d8763a7beeaebb171b4dec01fbe3
Grammy Jiang <grammy.jiang@gmail.com>
2020-05-12 15:59:06 +1000

    Add an empty class as a base class of Settings

--- bifrost/settings/__init__.py
+++ bifrost/settings/__init__.py
@@ -1,4 +1,5 @@
 import logging
+from collections import MutableMapping, namedtuple
 from typing import Dict
 
 # The pair of priority and priority_value
@@ -11,3 +12,53 @@ PRIORITIES: Dict[str, int] = {
 }
 
 logger = logging.getLogger(__name__)  # pylint: disable = invalid-name
+
+Setting = namedtuple("Setting", ["priority", "priority_value", "value"])
+
+
+class BaseSettings(MutableMapping):
+    """
+
+    """
+
+    def __init__(self):
+        """
+
+        """
+
+    def __getitem__(self, k):
+        """
+
+        :return:
+        """
+
+    def __setitem__(self, k, v):
+        """
+
+        :param item:
+        :return:
+        """
+
+    def __delitem__(self, v):
+        """
+
+        :return:
+        """
+
+    def __iter__(self):
+        """
+
+        :return:
+        """
+
+    def __len__(self):
+        """
+
+        :return:
+        """
+
+    def __contains__(self, o):
+        """
+
+        :return:
+        """
