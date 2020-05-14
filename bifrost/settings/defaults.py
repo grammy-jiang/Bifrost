d7a40965abcdd1ee4ffb5f852ba3960742e53c54
Grammy Jiang <grammy.jiang@gmail.com>
2020-05-15 09:44:13 +1000

    Add logging default settings

--- bifrost/settings/defaults.py
+++ bifrost/settings/defaults.py
@@ -1,1 +1,10 @@
-LOOP = "uvloop"
+"""
+Default settings
+"""
+import logging
+
+# ==== LOG CONFIGURATION ======================================================
+
+LOG_LEVEL = logging.DEBUG
+LOG_FORMATTER_FMT = "%(asctime)-15s [%(name)s] %(levelname)-8s: %(message)s"
+LOG_FORMATTER_DATEFMT = "%Y-%m-%d %H:%M:%S"
