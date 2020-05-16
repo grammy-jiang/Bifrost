8d4120db8af1b021eca4f6376eed06476a46c265
Grammy Jiang <grammy.jiang@gmail.com>
2020-05-17 00:21:05 +1000

    Add a setting in default settings

--- bifrost/settings/defaults.py
+++ bifrost/settings/defaults.py
@@ -8,3 +8,8 @@ import logging
 LOG_LEVEL = logging.DEBUG
 LOG_FORMATTER_FMT = "%(asctime)-15s [%(name)s] %(levelname)-8s: %(message)s"
 LOG_FORMATTER_DATEFMT = "%Y-%m-%d %H:%M:%S"
+
+# ==== MODE ===================================================================
+
+# the role that bifrost will play; it could be server or client
+ROLE = "server"
