8959f5fa7a1656c73c73a58e19a58abd0c0ca6cd
Grammy Jiang <grammy.jiang@gmail.com>
2020-05-17 21:39:57 +1000

    Remove tailing space in logging message format

--- bifrost/settings/defaults.py
+++ bifrost/settings/defaults.py
@@ -6,7 +6,7 @@ import logging
 # ==== LOG CONFIGURATION ======================================================
 
 LOG_LEVEL = logging.DEBUG
-LOG_FORMATTER_FMT = "%(asctime)-15s [%(name)s] %(levelname)-8s: %(message)s"
+LOG_FORMATTER_FMT = "%(asctime)s [%(name)s] %(levelname)s: %(message)s"
 LOG_FORMATTER_DATEFMT = "%Y-%m-%d %H:%M:%S"
 
 # ==== CORE MODULES ===========================================================
