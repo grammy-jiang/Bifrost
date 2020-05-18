faeca317d5ce45cb2a472de1f77aea117309024d
Grammy Jiang <grammy.jiang@gmail.com>
2020-05-18 19:34:53 +1000

    Add middlewares and extensions load in managers

--- bifrost/extensions/manager.py
+++ bifrost/extensions/manager.py
@@ -4,10 +4,12 @@ Extension Manager
 from __future__ import annotations
 
 import logging
+import pprint
 from typing import Type
 
 from bifrost.service import Service
 from bifrost.settings import Settings
+from bifrost.utils.misc import load_object
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +27,17 @@ class ExtensionManager:
         :param settings:
         :type settings: Settings
         """
+        self.service = service
+        self.settings = settings
+
+        self.cls_extensions = dict(
+            sorted(self.settings["EXTENSIONS"].items(), key=lambda items: items[1])
+        )
+        self.obj_extensions = [
+            load_object(cls).from_service(self.service)
+            for cls in self.cls_extensions.keys()
+        ]
+        logger.info("Enabled extensions: \n%s", pprint.pformat(self.cls_extensions))
 
     @classmethod
     def from_service(cls, service: Type[Service]) -> ExtensionManager:
--- bifrost/middlewares/manager.py
+++ bifrost/middlewares/manager.py
@@ -4,10 +4,12 @@ Middleware Manager
 from __future__ import annotations
 
 import logging
+import pprint
 from typing import Type
 
 from bifrost.service import Service
 from bifrost.settings import Settings
+from bifrost.utils.misc import load_object
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +27,17 @@ class MiddlewareManager:
         :param settings:
         :type settings: Settings
         """
+        self.service = service
+        self.settings = settings
+
+        self.cls_middlewares = dict(
+            sorted(self.settings["MIDDLEWARES"].items(), key=lambda items: items[1])
+        )
+        self.middlewares = [
+            load_object(cls).from_service(self.service)
+            for cls in self.cls_middlewares.keys()
+        ]
+        logger.info("Enabled middlewares: \n%s", pprint.pformat(self.cls_middlewares))
 
     @classmethod
     def from_service(cls, service: Type[Service]) -> MiddlewareManager:
