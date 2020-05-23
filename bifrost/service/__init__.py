dfc2fc62e66be98805f0b859bb0805e4e105a54b
Grammy Jiang <grammy.jiang@gmail.com>
2020-05-23 11:58:30 +1000

    Add loop and logging into Service

--- bifrost/service/__init__.py
+++ bifrost/service/__init__.py
@@ -1,6 +1,7 @@
 """
 Service module
 """
+import logging
 from asyncio.events import AbstractEventLoop
 from typing import TYPE_CHECKING, Type
 
@@ -10,6 +11,8 @@ from bifrost.utils.misc import load_object
 
 if TYPE_CHECKING:
     from bifrost.utils.manager import Manager
+
+logger = logging.getLogger(__name__)
 
 
 class Service:
@@ -24,16 +27,21 @@ class Service:
         :type settings: Settings
         """
         self.settings = settings
+        self.role: str = settings["ROLE"]
+        logger.info("This service is running in role: %s", self.role.upper())
 
-        self.extension_manager: Type["Manager"] = load_object(
+        self.loop: Type[AbstractEventLoop] = get_event_loop(settings)
+        logger.info(
+            "In this service the loop is adopted from: %s", settings["LOOP"].upper()
+        )
+
+        self.extension_manager: Type[Manager] = load_object(
             settings["MIDDLEWARE_MANAGER"]
         ).from_service(self)
 
-        self.middleware_manager: Type["Manager"] = load_object(
+        self.middleware_manager: Type[Manager] = load_object(
             settings["EXTENSION_MANAGER"]
         ).from_service(self)
-
-        self.loop: Type[AbstractEventLoop] = get_event_loop(settings)
 
     @classmethod
     def from_settings(cls, settings: Settings):
