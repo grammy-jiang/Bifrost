2037757f065412a5dd1e65cc0dab4a17068ef816
Grammy Jiang <grammy.jiang@gmail.com>
2020-05-29 21:24:15 +1000

    Add signals sending in BaseExtension

--- bifrost/extensions/__init__.py
+++ bifrost/extensions/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+from asyncio.events import AbstractEventLoop
 from typing import Type
 
+from bifrost import signals
 from bifrost.service import Service
 from bifrost.settings import Settings
 
@@ -9,12 +11,17 @@ from bifrost.settings import Settings
 class BaseExtension:
     def __init__(self, service: Type[Service], settings: Settings):
         self.service: Type[Service] = service
+        self.loop: Type[AbstractEventLoop] = service.loop
         self.settings: Settings = settings
 
     @classmethod
     def from_service(cls, service: Type[Service]) -> BaseExtension:
         settings: Settings = getattr(service, "settings")
         obj = cls(service, settings)
+
+        service.signal_manager.connect(obj.loop_started, signal=signals.loop_started)
+        service.signal_manager.connect(obj.loop_stopped, signal=signals.loop_stopped)
+
         return obj
 
     def loop_started(self, sender):
