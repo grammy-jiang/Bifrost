b40bad06ce46635c272636a7dd5e39bc61dd28e2
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-02 15:58:17 +1000

    Fix the stats format in LogStats

--- bifrost/extensions/logstats.py
+++ bifrost/extensions/logstats.py
@@ -60,8 +60,8 @@ class LogStats(BaseExtension):
         :return:
         :rtype: None
         """
-        self.stats["data_sent"] = 0
-        self.stats["data_received"] = 0
+        self.stats["data/sent"] = 0
+        self.stats["data/received"] = 0
 
         self.log(self.loop)
 
@@ -74,48 +74,48 @@ class LogStats(BaseExtension):
                 "Service details:\n%s",
                 pprint.pformat(
                     {
-                        "data_sent": "{:,.3f} {}".format(
-                            *convert_unit(self.stats["data_sent"]),
+                        "data/sent": "{:,.3f} {}".format(
+                            *convert_unit(self.stats["data/sent"]),
                         ),
-                        "data_received": "{:,.3f} {}".format(
-                            *convert_unit(self.stats["data_received"]),
+                        "data/received": "{:,.3f} {}".format(
+                            *convert_unit(self.stats["data/received"]),
                         ),
-                        "time_start": self.service.start_time.strftime(
+                        "time/start": self.service.start_time.strftime(
                             "%Y-%m-%d %H:%M:%S"
                         ),
-                        "time_end": end_time.strftime("%Y-%m-%d %H:%M:%S"),
-                        "time_running": str(end_time - self.service.start_time),
+                        "time/end": end_time.strftime("%Y-%m-%d %H:%M:%S"),
+                        "time/running": str(end_time - self.service.start_time),
                     }
                 ),
             )
 
     def data_sent(self, sender: Any, data: bytes):
-        self.stats["data_sent"] += len(data)
+        self.stats["data/sent"] += len(data)
 
     def data_received(self, sender, data: bytes):
-        self.stats["data_received"] += len(data)
+        self.stats["data/received"] += len(data)
 
     def log(self, loop):
         inbound_rate = int(
-            (self.stats["data_received"] - self._data_received) / self.interval * 8
+            (self.stats["data/received"] - self._data_received) / self.interval * 8
         )
         outbound_rate = int(
-            (self.stats["data_sent"] - self._data_sent) / self.interval * 8
+            (self.stats["data/sent"] - self._data_sent) / self.interval * 8
         )
 
         logger.info(
             "Data sent: %s, received: %s",
             "[{:,.3f}] {} (at [{:,.3f}] {})".format(
-                *convert_unit(self.stats["data_sent"]),
+                *convert_unit(self.stats["data/sent"]),
                 *convert_unit(outbound_rate, rate=True),
             ),
             "[{:,.3f}] {} (at [{:,.3f}] {})".format(
-                *convert_unit(self.stats["data_received"]),
+                *convert_unit(self.stats["data/received"]),
                 *convert_unit(inbound_rate, rate=True),
             ),
         )
 
-        self._data_sent = self.stats["data_sent"]
-        self._data_received = self.stats["data_received"]
+        self._data_sent = self.stats["data/sent"]
+        self._data_received = self.stats["data/received"]
 
         self.task: TimerHandle = loop.call_later(self.interval, self.log, loop)
