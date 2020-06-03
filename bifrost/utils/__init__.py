6268432cb5b5354d2e985b9b41b55e572e18581d
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-03 16:24:43 +1000

    Move configure_logging to log module

--- bifrost/utils/__init__.py
+++ bifrost/utils/__init__.py
@@ -1,24 +1,16 @@
-import logging
-
+"""
+Utils
+"""
 from bifrost.settings import Settings, defaults
 
 
-def get_settings():
-    settings = Settings()
-    with settings.unfreeze(priority="default") as settings_:
-        settings_.update_from_module(defaults)
+def get_settings() -> Settings:
+    """
+    Get default settings
+    :return:
+    :rtype: Settings
+    """
+    settings: Settings = Settings()
+    with settings.unfreeze(priority="default") as _settings:
+        _settings.update_from_module(defaults)
     return settings
-
-
-def configure_logging(settings: Settings):
-    # Get a console handler and configure it
-    console_handler = logging.StreamHandler()
-
-    formatter = logging.Formatter(
-        fmt=settings["LOG_FORMATTER_FMT"], datefmt=settings["LOG_FORMATTER_DATEFMT"],
-    )
-    console_handler.setFormatter(formatter)
-    console_handler.setLevel(settings["LOG_LEVEL"])
-
-    # add this console handler into logging
-    logging.root.addHandler(console_handler)
---
+++ bifrost/utils/log.py
@@ -0,0 +1,55 @@
+import logging
+import platform
+import pprint
+import ssl
+
+from bifrost.settings import Settings
+
+logger = logging.getLogger(__name__)
+
+
+def configure_logging(settings: Settings):
+    # Get a console handler and configure it
+    console_handler = logging.StreamHandler()
+
+    formatter = logging.Formatter(
+        fmt=settings["LOG_FORMATTER_FMT"], datefmt=settings["LOG_FORMATTER_DATEFMT"],
+    )
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(settings["LOG_LEVEL"])
+
+    # add this console handler into logging
+    logging.root.addHandler(console_handler)
+
+
+def get_runtime_info() -> None:
+    """
+    Log the environment status
+    :return:
+    :rtype: None
+    """
+    logger.info(
+        "Platform: %(platform)s", {"platform": pprint.pformat(platform.platform())}
+    )
+    logger.info(
+        "Platform details:\n%s",
+        pprint.pformat(
+            {
+                "OpenSSL": ssl.OPENSSL_VERSION,
+                "architecture": platform.architecture(),
+                "machine": platform.machine(),
+                "node": platform.node(),
+                "processor": platform.processor(),
+                "python_build": platform.python_build(),
+                "python_compiler": platform.python_compiler(),
+                "python_branch": platform.python_branch(),
+                "python_implementation": platform.python_implementation(),
+                "python_revision": platform.python_revision(),
+                "python_version": platform.python_version(),
+                "release": platform.release(),
+                "system": platform.system(),
+                "version": platform.version(),
+            }
+        ),
+    )
+    logger.info("Versions:\n%s", pprint.pformat({"Python": platform.python_version()}))
