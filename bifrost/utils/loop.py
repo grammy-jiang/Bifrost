61653e5114fbbcb7134a41cf18cc73a3f21b8b20
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-03 15:16:13 +1000

    Update get_event_loop in misc

--- bifrost/utils/loop.py
+++ bifrost/utils/loop.py
@@ -1,24 +1,34 @@
+"""
+Create a singleton object for asyncio loop
+"""
 from asyncio.events import AbstractEventLoop
-from typing import Type
+from typing import Optional, Type
 
 from bifrost.settings import Settings
 from bifrost.utils.misc import load_object
 
-_loop = None
+__loop: Optional[Type[AbstractEventLoop]] = None
 
 
 def get_event_loop(
-    settings: Settings, func: str = "new_event_loop"
+    settings: Settings, func: str = "new_event_loop", *args, **kwargs
 ) -> Type[AbstractEventLoop]:
     """
-
+    Return a singleton object for asyncio loop
+    :param settings:
+    :type settings: Settings
+    :param func:
+    :type func: str
+    :param args:
+    :param kwargs:
     :return:
-    :rtype: Type[BaseEventLoop]
+    :rtype: Type[AbstractEventLoop]
     """
-    global _loop
-    if _loop is None:
+
+    global __loop
+
+    if __loop is None:
         loop_path: str = ".".join([settings["LOOP"], func])
-        _loop = load_object(loop_path)
-        return _loop()
-    else:
-        return _loop
+        __loop = load_object(loop_path)(*args, **kwargs)
+
+    return __loop
