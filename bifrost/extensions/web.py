a6f111a9b2b39f1fef751f3fd463f34f8d341672
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-04 15:31:34 +1000

    Add an empty example with Sanic in Web extension

--- bifrost/extensions/web.py
+++ bifrost/extensions/web.py
@@ -1,16 +1,31 @@
 import logging
 from typing import Any
 
+from sanic import Sanic
+from sanic.request import Request
+from sanic.response import json
+
 from bifrost.extensions import BaseExtension
 from bifrost.service import Service
 from bifrost.settings import Settings
 
 logger = logging.getLogger(__name__)
 
+app = Sanic("Bifrost Web")
+
+
+@app.route("/")
+async def test(request: Request):
+    return json({"hello": "world"})
+
 
 class Web(BaseExtension):
     def __init__(self, service: Service, settings: Settings):
         super(Web, self).__init__(service, settings)
+
+        self.server = app.create_server(
+            host="0.0.0.0", port=8000, return_asyncio_server=True
+        )
 
     def service_started(self, sender: Any) -> None:
         """
@@ -20,6 +35,7 @@ class Web(BaseExtension):
         :return:
         :rtype: None
         """
+        self._loop.create_task(self.server)
         logger.info("Extension [%s] is running...", self.__class__.__name__)
 
     def service_stopped(self, sender: Any) -> None:
