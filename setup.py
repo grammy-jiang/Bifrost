f950cf219d478cd1713b8b6c17c33c1ccce5cd2d
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-08 23:11:09 +1000

    Fill more information in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 import versioneer
 
-setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
+setup(
+    name="Bifrost",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="A Great FireWall Crossing Framework",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=False,
+    zip_safe=True,
+    python_requires=">=3.8",
+    install_requires=["sanic", "uvloop"],
+)
