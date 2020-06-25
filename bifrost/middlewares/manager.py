219eec300a11dbc15b6ecf8a31e9006d8a044005
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-25 22:42:10 +1000

    Add a property middlewares to MiddlewareManager

--- bifrost/middlewares/manager.py
+++ bifrost/middlewares/manager.py
@@ -5,13 +5,13 @@ from __future__ import annotations
 
 import logging
 import pprint
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Dict
 
-from bifrost.settings import Settings
 from bifrost.utils.manager import Manager
 
 if TYPE_CHECKING:
     from bifrost.service import Service
+    from bifrost.settings import Settings
 
 logger = logging.getLogger(__name__)
 
@@ -33,3 +33,22 @@ class MiddlewareManager(Manager):
 
         self._register_components("MIDDLEWARES")
         logger.info("Enabled middlewares: \n%s", pprint.pformat(self._cls_components))
+
+    @property
+    def middlewares(self) -> Dict[str, object]:
+        """
+        get all middlewares in a dict
+        :return:
+        :rtype: Dict[str, object]
+        """
+        return self._components
+
+    def get_middleware(self, name: str) -> object:
+        """
+        get a middleware by its name
+        :param name:
+        :type name: str
+        :return:
+        :rtype: object
+        """
+        return self.middlewares[name]
