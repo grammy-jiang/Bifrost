cc802d511fdba844db4149f73cba6472d23c70ea
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-25 20:54:43 +1000

    Add a base exception for Service

---
+++ bifrost/exceptions/service.py
@@ -0,0 +1,10 @@
+"""
+Exceptions used in Service
+"""
+from bifrost.exceptions import BifrostException
+
+
+class ServiceException(BifrostException):
+    """
+    The base exception used in this module
+    """
