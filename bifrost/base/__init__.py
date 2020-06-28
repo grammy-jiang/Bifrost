f70622729fff4b57242848ddc76f9bb35ae3656e
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-28 14:10:40 +1000

    Update __init__ in base package

--- bifrost/base/__init__.py
+++ bifrost/base/__init__.py
@@ -1,4 +1,8 @@
+"""
+All of base and MIXIN classes
+"""
 from bifrost.base.component import BaseComponent
+from bifrost.base.log import LoggerMixin
 from bifrost.base.manager import BaseManager
 
-__all__ = ["BaseComponent", "BaseManager"]
+__all__ = ["BaseComponent", "BaseManager", "LoggerMixin"]
