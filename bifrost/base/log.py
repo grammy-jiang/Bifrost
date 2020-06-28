87924a616e368f32e0a16d9d23edfbd9e871eac4
Grammy Jiang <grammy.jiang@gmail.com>
2020-06-28 14:09:22 +1000

    Add docstring in LoggerMixin

--- bifrost/base/log.py
+++ bifrost/base/log.py
@@ -1,8 +1,19 @@
+"""
+Logger Mixin
+"""
 import logging
 
 
-class LoggerMixin(object):
+class LoggerMixin:  # pylint: disable=too-few-public-methods
+    """
+    Logger Mixin
+    """
+
     @property
     def logger(self):
+        """
+        Logger
+        :return:
+        """
         name = ".".join([self.__module__, self.__class__.__name__])
         return logging.getLogger(name)
