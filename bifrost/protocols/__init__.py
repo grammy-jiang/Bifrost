feb687d288eb6359ecf540d8f933decad618b629
Grammy Jiang <grammy.jiang@gmail.com>
2020-07-04 21:45:11 +1000

    Fix log message in Client Protocol

--- bifrost/protocols/__init__.py
+++ bifrost/protocols/__init__.py
@@ -26,16 +26,17 @@ class Client(ProtocolMixin, Protocol, LoggerMixin):
         :return:
         :rtype: None
         """
-        if not self.config["CLIENT_SSL_CERT_FILE"]:
-            self.logger.debug(
-                "[CONN] [%s:%s] connected", *transport.get_extra_info("peername")[:2]
-            )
-        else:
+        if cipher := transport.get_extra_info("cipher"):
             self.logger.debug(
                 "[CONN] [%s:%s] connected with name [%s], version [%s], secret bits [%s]",
                 *transport.get_extra_info("peername")[:2],
-                *transport.get_extra_info("cipher"),
+                *cipher,
             )
+        else:
+            self.logger.debug(
+                "[CONN] [%s:%s] connected", *transport.get_extra_info("peername")[:2]
+            )
+
         self.transport = transport
 
     def data_received(self, data: bytes) -> None:
