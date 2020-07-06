d2d7d65ed14639d22d7496580ef4baf8fae2372c
Grammy Jiang <grammy.jiang@gmail.com>
2020-07-06 16:45:49 +1000

    Add an empty method in Socks5Mixin

--- bifrost/base/protocols/socks5.py
+++ bifrost/base/protocols/socks5.py
@@ -7,3 +7,12 @@ class Socks5Mixin:
     """
     Socks5 Protocol Mixin
     """
+
+    def process_request(self, data: bytes) -> bytes:
+        """
+
+        :param data:
+        :type data: bytes
+        :return:
+        :rtype: bytes
+        """
