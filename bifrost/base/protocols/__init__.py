8a1922d1f7b8f2b51d90d6dbddfba0cf254acc6e
Grammy Jiang <grammy.jiang@gmail.com>
2020-07-06 16:44:04 +1000

    Add Socks5Mixin in bifrost.base.protocols

---
+++ bifrost/base/protocols/__init__.py
@@ -0,0 +1,3 @@
+from bifrost.base.protocols.socks5 import Socks5Mixin
+
+__all__ = ["Socks5Mixin"]
---
+++ bifrost/base/protocols/socks5.py
@@ -0,0 +1,9 @@
+"""
+Socks5 Protocol Mixin
+"""
+
+
+class Socks5Mixin:
+    """
+    Socks5 Protocol Mixin
+    """
