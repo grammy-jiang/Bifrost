e9c45077f48e8bf7455c9c7ccd187c398f74bb1c
Grammy Jiang <grammy.jiang@gmail.com>
2020-07-07 16:47:43 +1000

    Add two functions to convert text to bytes and str

--- bifrost/utils/misc.py
+++ bifrost/utils/misc.py
@@ -2,6 +2,7 @@
 Miscellaneous
 """
 from importlib import import_module
+from typing import Union
 
 
 def load_object(path):
@@ -15,3 +16,49 @@ def load_object(path):
     mod = import_module(module)
 
     return getattr(mod, name)
+
+
+def to_str(text: Union[bytes, str], encoding="utf-8", errors="strict") -> str:
+    """
+    Convert text to str.
+
+    Refer to:
+    https://docs.python.org/3/library/stdtypes.html#bytes.decode
+
+    :param text:
+    :type text: Union[bytes, str]
+    :param encoding:
+    :type encoding: str
+    :param errors:
+    :type errors: str
+    :return:
+    :rtype: str
+    """
+    if isinstance(text, bytes):
+        return text.decode(encoding, errors)
+    elif isinstance(text, str):
+        return text
+    raise TypeError
+
+
+def to_bytes(text: Union[bytes, str], encoding="utf-8", errors="strict") -> bytes:
+    """
+    Convert text to bytes.
+
+    Refer to:
+    https://docs.python.org/3/library/stdtypes.html#str.encode
+
+    :param text:
+    :type text: Union[bytes, str]
+    :param encoding:
+    :type encoding: str
+    :param errors:
+    :type errors: str
+    :return:
+    :rtype: bytes
+    """
+    if isinstance(text, str):
+        return text.encode(encoding, errors)
+    elif isinstance(text, bytes):
+        return text
+    raise TypeError
--- tests/utils/misc.py
+++ tests/utils/misc.py
@@ -1,9 +1,21 @@
 from unittest.case import TestCase
 
-from bifrost.utils.misc import load_object
+from bifrost.utils.misc import load_object, to_bytes, to_str
 
 
 class MiscTestCase(TestCase):
     def test_load_object(self):
         obj = load_object("bifrost.utils.misc.load_object")
         self.assertIs(obj, load_object)
+
+    def test_to_bytes(self):
+        self.assertEqual(to_bytes("a"), b"a")
+        self.assertEqual(to_bytes(b"a"), b"a")
+        with self.assertRaises(TypeError):
+            to_bytes(0)
+
+    def test_to_str(self):
+        self.assertEqual(to_str(b"a"), "a")
+        self.assertEqual(to_str("a"), "a")
+        with self.assertRaises(TypeError):
+            to_str(0)
